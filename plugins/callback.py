8cc68c9e8488ca71a0da20f8508ee29cdb36f829
teamdabang <dgjg45768@gmail.com>
2025-01-06 09:09:51 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -21,7 +21,7 @@ async def about(bot,update):
 async def donatecm(bot,message):
     text = script.DONATE_TXT
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin",url = "https://t.me/CallAdminRobot"),
+        [InlineKeyboardButton("🦋 Admin",url = "https://t.me/King18x007"),
         InlineKeyboardButton("✖️ Close",callback_data = "cancel") ]])
     await message.reply_text(text = text,reply_markup = keybord)    
 
@@ -78,11 +78,11 @@ async def donate(bot,update):
 async def home_callback_handler(bot, query):
     text = f"""Hello {query.from_user.mention} \n\n➻ This Is An Advanced And Yet Powerful Rename Bot.\n\n➻ Using This Bot You Can Rename And Change Thumbnail Of Your Files.\n\n➻ You Can Also Convert Video To File Aɴᴅ File To Video.\n\n➻ This Bot Also Supports Custom Thumbnail And Custom Caption.\n\n<b>Bot Is Made By @Madflix_Bots</b>"""
     keybord = InlineKeyboardMarkup([  
-                    [InlineKeyboardButton("📢 Updates", url="https://t.me/Madflix_Bots"),
-                    InlineKeyboardButton("💬 Support", url="https://t.me/MadflixBots_Support")],
+                    [InlineKeyboardButton("📢 Updates", url="https://t.me/Aniket_Movies_Hub"),
+                    InlineKeyboardButton("💬 Support", url="https://t.me/King18x007")],
                     [InlineKeyboardButton("🛠️ Help", callback_data='help'),
 		            InlineKeyboardButton("❤️‍🩹 About", callback_data='about')],
-                    [InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url="https://t.me/CallAdminRobot")]
+                    [InlineKeyboardButton("⏤͟͟͞͞ 🇮🇳 𝐀𝐧𝐢𝐤𝐞𝐭 𝐂𝐨𝐝𝐞𝐫 ᯤ̸ 🧑‍💻", url="https://t.me/King18x007")]
 		  ])
     await query.message.edit_text(text=text, reply_markup=keybord)
 
