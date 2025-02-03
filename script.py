7dd37b3486b9738cbaff064ba26480dead63ccfc
teamdabang <dgjg45768@gmail.com>
2025-02-03 07:44:46 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,70 @@
 class script(object):
-OTTS_TXT
+   OTTS_TXT = "Available OTTs
+
+1. ALTT
+2. ARY Digital
+3. Addatimes
+4. Aha
+5. Amazon miniTV
+6. Atrangii
+7. Bongo BD
+8. Chaupal
+9. Chorki
+10. Dangal Play
+11. Discovery+
+12. EPIC ON
+13. ETV Win
+14. Eros Now
+15. HBO Max
+16. Hoichoi
+17. Hotstar
+18. Hulu
+19. Hungama
+20. JioCinema
+21. KLiKK
+22. Kuku FM
+25. Lionsgate Play
+24. MUBI
+26. MX Player
+27. ManoramaMAX
+28. Netflix
+29. Oho Gujarati
+30. Planet Marathi
+31. Pocket FM
+32. Prime Video
+33. PrimeShots
+34. ReelDrama
+35. STAGE
+36. Saina Play
+37. ShemarooMe
+38. SonyLiv
+39. Sun NXT
+40. Tarang Plus
+41. ULLU
+42. Ultra Jhakaas
+43. Vivamax
+44. Zee5
+
+More OTTs Coming Soon... 😘❤️"
+
+    ContactUs_TXT = "Contact: @x1xEdiz"
+
+    
+    usage_TXT = "Hi I am one and only DRM Downloader Bot on Telegram.
+
+💥 Send Any DRM Link I will upload it To Telegram or Google Drive.
+
+💥 I support Direct DRM link from Zee5, Hotstar, SonyLIV, JioCinema, Sunnxt, Atrangii, Etc....
+
+💥 You can also download from custom mpd link if you have the keys."
+
+
+
+    
+    plans_TXT = "📞 Contact For Plan Details: @x1xEdiz"
+
+     
+
     
     HELP_TXT = """<b>Hey</b> {}
     
